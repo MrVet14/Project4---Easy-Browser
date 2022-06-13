cdb9c8bcc5fb79ac1de2aa70c3f61632d0b53711
Vitali <vitali.vyucheiski@gmail.com>
2022-06-13 10:42:10 +0300

    Initial commit

---
+++ Project4 - Easy Browser/TableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TableViewController.swift
+//  Project4 - Easy Browser
+//
+//  Created by Vitali Vyucheiski on 3/5/22.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Project4---Easy-Browser
+ A Learning Project 4, coded by Me during Hacking with Swift in 100 Days Course
